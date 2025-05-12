154c6beffd5a7d2cce8006fc48223d46f26f0146
Wojtek2006 <118116112+Wojtek2006@users.noreply.github.com>
2025-05-12 12:28:42 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,96 @@
-$( document ).ready(function() {
-    let imageSources = ['Ciri.png', 'Geralt.png', 'Iorveth.png', 'Jaskier.png', 'Triss.png', 'Vesemir.png', 'Yennefer.png', 'Zoltan.png',
-                        'Ciri.png', 'Geralt.png', 'Iorveth.png', 'Jaskier.png', 'Triss.png', 'Vesemir.png', 'Yennefer.png', 'Zoltan.png'];
+$(document).ready(function () {
+  let imageSources = [
+    "Ciri.png",
+    "Geralt.png",
+    "Iorveth.png",
+    "Jaskier.png",
+    "Triss.png",
+    "Vesemir.png",
+    "Yennefer.png",
+    "Zoltan.png",
+    "Ciri.png",
+    "Geralt.png",
+    "Iorveth.png",
+    "Jaskier.png",
+    "Triss.png",
+    "Vesemir.png",
+    "Yennefer.png",
+    "Zoltan.png",
+  ];
 
-    let shuffledImageSources = shuffle(imageSources);
-    
-    loadImages(shuffledImageSources);
+  let shuffledImageSources = shuffle(imageSources);
+
+  loadImages(shuffledImageSources);
+});
+
+let previousCardId;
+let oneFlipped = false;
+let turnCount = 0;
+let score = 0;
+
+// NIGGER
+
+$(".card").click(function (e) {
+  currentCardId = $(this).attr('id');
+
+  $('.card__face--back', this).toggleClass('flipped');
+
+
+  if (oneFlipped) {
+    turnCount++;
+    oneFlipped = false;
+
+    let previousCardImage = $('#' + previousCardId + ' .card__face--front').attr('src');
+    let currentCardImage = $('#' + currentCardId + ' .card__face--front').attr('src')
+
+    if (currentCardImage === previousCardImage) {
+
+      flipPair(previousCardId, currentCardId);
+
+    }
+    else {
+
+      hidePair(previousCardId, currentCardId);
+
+    }
+  }
+  else {
+    oneFlipped = true;
+    previousCardId = $(this).attr('id');
+  }
 
 });
 
-let oneFlipped = false;
-let turnCount = 0;
-
-$('.card__face--back').click(function(e){
-    // e.preventDefault();
-    $(this).toggleClass('flipped');
-})
-
-
-
 function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
 
-    return array;
+  return array;
 }
 
 function loadImages(images) {
-    for (let i = 1; i <= images.length; i++) {
-        $('#card' + i + ' .card__face--front').attr('src', 'images/Humans/' + images[i-1]);
-    }
+  for (let i = 1; i <= images.length; i++) {
+    $("#card" + i + " .card__face--front").attr(
+      "src",
+      "images/Humans/" + images[i - 1]
+    );
+  }
+}
+
+function flipPair(prevCardId, CurrCardId) {
+  setTimeout(function() {
+    $('#' + prevCardId).css({'opacity' : '0'});
+    $('#' + CurrCardId).css({'opacity' : '0'});
+    }, 1000);
+}
+
+function hidePair(prevCardId, CurrCardId) {
+  setTimeout(function() {
+    $('#' + prevCardId + ' .card__face--back').toggleClass('flipped');
+    $('#' + CurrCardId + ' .card__face--back').toggleClass('flipped');
+    }, 1000);
 }
